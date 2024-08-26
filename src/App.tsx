d87c07d640b34067fd71961198f67b3b35ff7016
Sapna <sapnasawai1999@gmail.com>
2024-08-26 17:32:16 +0530

    splash screen implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import TabNavigator from './TabNavigator';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide()
+  })
   return (
     <>
       <StatusBar barStyle="dark-content" />
